2c6e100eb1b8dd7e4d510f9544ab5da1dca86287
Dustin Kut Moy Cheung <dcheung@redhat.com>
2016-07-27 23:20:54 -0400

    Switch to 'use-package'

--- init.el
+++ init.el
@@ -1,5 +1,53 @@
-; list the packages you want
-(setq package-list '(evil helm magit evil-magit))
+;;; init.el --- Dustin's emacs config
+;;; Commentary:
+;;; Dustin's lame Emacs config
+
+;;; Code:
+
+;; load the package manager
+(require 'package)
+
+;; Add Emacs package repositories to the list of available repositories
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(package-initialize)
+
+;; fetch the list of packages available
+(unless package-archive-contents
+    (package-refresh-contents))
+
+;;; install use-package, this will install the other plugins as required
+(unless (package-installed-p 'use-package)
+    (package-install 'use-package))
+
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+(use-package helm
+  :ensure t
+  :config
+  (helm-mode 1)
+  :bind (("M-x" . helm-M-x)))
+
+(use-package magit
+  :ensure t)
+  
+(use-package evil-magit
+  :ensure t)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package noctilux-theme
+  :ensure t)
+
+(use-package projectile
+  :ensure t
+  :init (projectile-global-mode))
 
 ;; disable menubar
 (menu-bar-mode -1)
@@ -16,54 +64,19 @@
 ;; show matching parentheses
 (show-paren-mode 1)
 
-;; load the package manager
-(require 'package)
-
-;; Add Emacs package repositories to the list of available repositories
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(package-initialize)
-
-;; fetch the list of packages available
-(unless package-archive-contents
-    (package-refresh-contents))
-
-;; install the missing packages
-(dolist (package package-list)
-    (unless (package-installed-p package)
-        (package-install package)))
-
-;; evil plugin
-(require 'evil)
-(evil-mode 1)
+(provide 'init)
+;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil-magit evil))))
+ '(package-selected-packages
+   (quote
+    (noctilux-theme use-package helm flycheck evil-magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(require 'helm)
-(helm-mode 1)
-
-;; Use helm-M-x instead of regular M-x, because eunderhill says it's better
-(global-set-key (kbd "M-x") 'helm-M-x)
-
-(global-set-key (kbd "C-x g") 'magit-status)
-
-
-;; evil-magit plugin
-(require 'evil-magit)
-
-;; themes
-(add-to-list 'custom-theme-load-path "~/.emacs.d/lib/color-themes")
-(load-theme 'noctilux t)
-
