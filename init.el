88b616de9ba440af935cbc611cc4984da1072afb
Dustin Kut Moy Cheung <dcheung@redhat.com>
2016-08-10 16:28:43 -0400

    Don't show startupscreen

--- init.el
+++ init.el
@@ -96,6 +96,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
     (company-mode noctilux-theme use-package helm flycheck evil-magit))))
